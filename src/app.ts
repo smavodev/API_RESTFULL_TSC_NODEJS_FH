8c64f822f7e6312625f02115dfca6917906fa90e
Sergio Villagomez <martin.v.ortega@gmail.com>
2024-07-29 22:42:20 -0500

    feat(mongodb): configuracion de conexion a mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import { envs } from './config/envs';
+import { MongoDatabase } from './data';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
-
 
 (async()=> {
   main();
 })();
 
-
 async function main() {
+
+  await MongoDatabase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    mongoUrl: envs.MONGO_URL,
+  });
 
   const server = new Server({
     port: envs.PORT,
@@ -16,4 +20,5 @@ async function main() {
   });
 
   server.start();
+
 }
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,6 +6,9 @@ export const envs = {
 
   PORT: get('PORT').required().asPortNumber(),
 
+  MONGO_URL: get('MONGO_URL').required().asString(),
+  MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
+
 }
 
 
